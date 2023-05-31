a434a372fd2a066708d72525c333ca2d0693c3b6
xioneko <stnecat@gmail.com>
2023-05-31 22:16:04 +0800

    🐛 Fix #4

--- app/src/main/java/com/xioneko/android/nekoanime/ui/search/screen/SearchHistoryView.kt
+++ app/src/main/java/com/xioneko/android/nekoanime/ui/search/screen/SearchHistoryView.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -29,7 +30,6 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.xioneko.android.nekoanime.data.model.SearchRecord
 import com.xioneko.android.nekoanime.ui.theme.NekoAnimeIcons
-import com.xioneko.android.nekoanime.ui.theme.basicWhite
 import com.xioneko.android.nekoanime.ui.theme.neutral06
 import com.xioneko.android.nekoanime.ui.theme.neutral10
 import com.xioneko.android.nekoanime.ui.theme.pink95
@@ -44,69 +44,70 @@ internal fun SearchHistoryView(
 ) {
     val searchHistory by source.collectAsStateWithLifecycle()
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(basicWhite)
-            .padding(15.dp, 8.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        if (searchHistory.isNotEmpty()) {
-            Text(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 12.dp),
-                text = "搜索历史",
-                textAlign = TextAlign.Start,
-                color = neutral10,
-                style = MaterialTheme.typography.bodyMedium
-            )
-            FlowRow(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.spacedBy(8.dp),
-            ) {
-                for (record in searchHistory.reversed()) {
-                    Box(
-                        modifier = Modifier
-                            .padding(bottom = 8.dp)
-                            .clip(CircleShape)
-                            .background(pink95.copy(alpha = 0.6f))
-                            .padding(10.dp, 6.dp)
-                            .clickable(
-                                interactionSource = remember { MutableInteractionSource() },
-                                indication = null,
-                                role = Role.Button,
-                                onClick = { onRecordClick(record.value) }
-                            ),
-                        contentAlignment = Alignment.Center,
-                    ) {
-                        Text(text = record.value, style = MaterialTheme.typography.bodySmall)
+    Surface {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(15.dp, 8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            if (searchHistory.isNotEmpty()) {
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 12.dp),
+                    text = "搜索历史",
+                    textAlign = TextAlign.Start,
+                    color = neutral10,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                FlowRow(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                ) {
+                    for (record in searchHistory.reversed()) {
+                        Box(
+                            modifier = Modifier
+                                .padding(bottom = 8.dp)
+                                .clip(CircleShape)
+                                .background(pink95.copy(alpha = 0.6f))
+                                .padding(10.dp, 6.dp)
+                                .clickable(
+                                    interactionSource = remember { MutableInteractionSource() },
+                                    indication = null,
+                                    role = Role.Button,
+                                    onClick = { onRecordClick(record.value) }
+                                ),
+                            contentAlignment = Alignment.Center,
+                        ) {
+                            Text(text = record.value, style = MaterialTheme.typography.bodySmall)
+                        }
                     }
                 }
-            }
 
-            Row(
-                modifier = Modifier
-                    .padding(top = 4.dp)
-                    .clickable(
-                        interactionSource = remember { MutableInteractionSource() },
-                        indication = null,
-                        role = Role.Button,
-                        onClick = onClearHistory,
-                    ),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Icon(
-                    painter = painterResource(NekoAnimeIcons.trash),
-                    contentDescription = null,
-                    tint = neutral06
-                )
-                Text(
-                    text = "清空搜索历史",
-                    color = neutral06,
-                    style = MaterialTheme.typography.bodySmall
-                )
+                Row(
+                    modifier = Modifier
+                        .padding(top = 4.dp)
+                        .clickable(
+                            interactionSource = remember { MutableInteractionSource() },
+                            indication = null,
+                            role = Role.Button,
+                            onClick = onClearHistory,
+                        ),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        painter = painterResource(NekoAnimeIcons.trash),
+                        contentDescription = null,
+                        tint = neutral06
+                    )
+                    Text(
+                        text = "清空搜索历史",
+                        color = neutral06,
+                        style = MaterialTheme.typography.bodySmall
+                    )
+                }
             }
         }
     }
